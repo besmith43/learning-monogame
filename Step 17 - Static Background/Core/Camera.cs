61184ecbe4ca5153f5b51a29a81a61c6bccf845d
Blake Smith <besmith43@gmail.com>
2020-05-05 19:51:39 -0500

    finished step 17

---
+++ Step 17 - Static Background/Core/Camera.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step_17___Static_Background
+{
+
+	public class Camera
+	{
+        public Matrix Transform { get; private set; }
+
+        public void Follow(Vector2 target)
+        {
+            var position = Matrix.CreateTranslation(
+              -target.X,
+              -target.Y,
+              0);
+
+            var offset = Matrix.CreateTranslation(400, 240, 0);
+
+            Transform = position * offset;
+        }
+    }
+}
---
+++ Step 17 - Static Background/Game1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Step_17___Static_Background
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Camera _camera;
+
+        private Texture2D _playerTexture;
+
+        private Texture2D _backgroundTexture;
+
+        private Vector2 _playerPosition;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            _camera = new Camera();
+
+            _playerTexture = Content.Load<Texture2D>("Square");
+            _backgroundTexture = Content.Load<Texture2D>("Background");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                _playerPosition.Y -= 3f;
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+                _playerPosition.Y += 3f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                _playerPosition.X -= 3f;
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+                _playerPosition.X += 3f;
+
+            _camera.Follow(_playerPosition);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(_backgroundTexture, new Vector2(0, 0), Color.White);
+
+            spriteBatch.End();
+
+            spriteBatch.Begin(transformMatrix: _camera.Transform);
+
+            spriteBatch.Draw(_playerTexture, _playerPosition, Color.Green);
+            spriteBatch.Draw(_playerTexture, new Vector2(0, 0), Color.Red);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
